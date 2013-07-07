7b9333cfb1c1f38ba0edc20e7c83be9c50270550
Sean Bolton <seanbolton.me@gmail.com>
2013-07-07 05:00:48 +0000

    Testing web.js error fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	var body = buf.toString(fs.readFileSync("index.html"));
+  	var body = new Buffer(fs.readFileSync("index.html"));
 	response.send(body);
 });
 
